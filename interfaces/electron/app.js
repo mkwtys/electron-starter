f8a2ef58fb71c1214dbc0ffce63577c8e7f31ae0
mkwtys <mkwtys@gmail.com>
2015-11-18 17:59:04 +0900

    chore: remove definitions

--- interfaces/electron/app.js
+++
@@ -1,4 +0,0 @@
-declare module app {
-  declare function on(event: string, cb: Function): void;
-  declare function quit(): void;
-}
--- interfaces/electron/browser-window.js
+++
@@ -1,8 +0,0 @@
-declare module 'browser-window' {
-  declare class exports {
-    constructor(options: Object): void;
-    on(event: string, cb: Function): void;
-    destroy(): void;
-    loadUrl(url: string, options?: Object): void;
-  }
-}
